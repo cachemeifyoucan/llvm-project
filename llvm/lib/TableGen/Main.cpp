196986fa293ff177e67bb34c61352aad84098deb
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2021-11-02 20:05:29 -0700

    Support: RealPathPrefixMapper => TreePathPrefixMapper
    Avoid doing full real path computations when mapping prefixes, leaving
    symlinks in file paths alone. This uses the new
    vfs::FileSystem::getDirectoryEntry().

--- llvm/lib/TableGen/Main.cpp
+++ llvm/lib/TableGen/Main.cpp
@@ -261,7 +261,7 @@ struct TableGenCache {
 
   SmallVector<MappedPrefix> PrefixMappings;
   BumpPtrAllocator Alloc;
-  Optional<RealPathPrefixMapper> PM;
+  Optional<TreePathPrefixMapper> PM;
   Optional<PrefixMapper> InversePM;
 
   TableGenCache() = default;
