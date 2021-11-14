8ea609e886dc0a269b159fab12c3cf13b0d0c1c9
Steven Wu <stevenwu@apple.com>
2021-11-13 18:37:34 -0800

    cas.o: Add basic support to ingest ELF object files
    Add basic support to convert ELF object file to cas object format.
    Currently, only flatv1 supports ELF.

--- llvm/lib/ExecutionEngine/JITLink/JITLink.cpp
+++ llvm/lib/ExecutionEngine/JITLink/JITLink.cpp
@@ -397,6 +397,8 @@ Expected<LinkGraph::GetEdgeKindNameFunction>
 getGetEdgeKindNameFunction(const Triple &TT) {
   if (TT.isOSBinFormatMachO())
     return getGetEdgeKindNameFunctionForMachO(TT);
+  if (TT.isOSBinFormatELF())
+    return getGetEdgeKindNameFunctionForELF(TT);
   return make_error<JITLinkError>("Unsupported triple");
 }
 
