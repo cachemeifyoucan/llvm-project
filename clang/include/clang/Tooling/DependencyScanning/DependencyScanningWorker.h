kkpan11/apple-llvm-project

clang-scan-deps: Mega-commit for dependency scanning on a CAS (split up!)
A number of things here, which should be split up and properly tested.
At least the following pieces make sense:

- Modifications to use a CachingOnDiskFileSystem. Maybe this should be
  optional.
- Using the result cache for minimized files.
- Modes to generate a CAS tree as output.

clang/include/clang/Tooling/DependencyScanning/DependencyScanningWorker.h
@@ -21,6 +21,12 @@
 #include "llvm/Support/FileSystem.h"
 #include <string>
 
+namespace llvm {
+namespace cas {
+class CachingOnDiskFileSystem;
+}
+} // namespace llvm
+
 namespace clang {
 
 class DependencyOutputOptions;
@@ -85,12 +91,23 @@ public:
                                   const CompilationDatabase &CDB,
                                   DependencyConsumer &Consumer);
 
+  /// Scan from a compiler invocation.
+  void computeDependenciesFromCompilerInvocation(
+      std::shared_ptr<CompilerInvocation> Invocation,
+      StringRef WorkingDirectory, DependencyConsumer &Consumer,
+      DiagnosticConsumer &DiagsConsumer);
+  void computeDependenciesFromCC1CommandLine(ArrayRef<const char *> Args,
+                                             StringRef WorkingDirectory,
+                                             DependencyConsumer &DepsConsumer);
+
+  llvm::cas::CachingOnDiskFileSystem &getRealFS() { return *RealFS; }
+
 private:
   IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts;
   std::shared_ptr<PCHContainerOperations> PCHContainerOps;
   std::unique_ptr<ExcludedPreprocessorDirectiveSkipMapping> PPSkipMappings;
 
-  llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> RealFS;
+  llvm::IntrusiveRefCntPtr<llvm::cas::CachingOnDiskFileSystem> RealFS;
   /// The file system that is used by each worker when scanning for
   /// dependencies. This filesystem persists accross multiple compiler
   /// invocations.
@@ -99,6 +116,7 @@ private:
   /// worker. If null, the file manager will not be reused.
   llvm::IntrusiveRefCntPtr<FileManager> Files;
   ScanningOutputFormat Format;
+  bool OverrideCASTokenCache;
 };
 
 } // end namespace dependencies

cachemeifyoucan/llvm-project

clang/include/clang/Tooling/DependencyScanning/DependencyScanningWorker.h
@@ -20,6 +20,12 @@
 #include "llvm/Support/FileSystem.h"
 #include <string>
 
+namespace llvm {
+namespace cas {
+class CachingOnDiskFileSystem;
+}
+} // namespace llvm
+
 namespace clang {
 
 class DependencyOutputOptions;
@@ -67,6 +73,17 @@ public:
                                   DependencyConsumer &Consumer,
                                   llvm::Optional<StringRef> ModuleName = None);
 
+  /// Scan from a compiler invocation.
+  void computeDependenciesFromCompilerInvocation(
+      std::shared_ptr<CompilerInvocation> Invocation,
+      StringRef WorkingDirectory, DependencyConsumer &Consumer,
+      DiagnosticConsumer &DiagsConsumer);
+  void computeDependenciesFromCC1CommandLine(ArrayRef<const char *> Args,
+                                             StringRef WorkingDirectory,
+                                             DependencyConsumer &DepsConsumer);
+
+  llvm::cas::CachingOnDiskFileSystem &getRealFS() { return *CacheFS; }
+
 private:
   std::shared_ptr<PCHContainerOperations> PCHContainerOps;
   std::unique_ptr<ExcludedPreprocessorDirectiveSkipMapping> PPSkipMappings;
@@ -75,6 +92,8 @@ private:
   llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> RealFS;
   /// The in-memory filesystem laid on top the physical filesystem in `RealFS`.
   llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> InMemoryFS;
+  /// The caching file system.
+  llvm::IntrusiveRefCntPtr<llvm::cas::CachingOnDiskFileSystem> CacheFS;
   /// The file system that is used by each worker when scanning for
   /// dependencies. This filesystem persists across multiple compiler
   /// invocations.
@@ -85,6 +104,7 @@ private:
   ScanningOutputFormat Format;
   /// Whether to optimize the modules' command-line arguments.
   bool OptimizeArgs;
+  bool OverrideCASTokenCache;
 };
 
 } // end namespace dependencies